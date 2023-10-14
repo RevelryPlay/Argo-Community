8d1013e68be0f5fdf3dba92f199727d2f6abff2b
Kylie Benfield <kylie@revelryplay.com>
2023-10-14 11:47:07 -0600

    Moving GL color set out of the update callback
    MacOS does not seem to like this being called from the callback

--- Common/System/Graphics/GLWindow.cpp
+++ Common/System/Graphics/GLWindow.cpp
@@ -2,63 +2,69 @@
 #include <functional>
 
 namespace Argo::System {
-GLWindow::GLWindow() = default;
-GLWindow::~GLWindow() { cleanup(); };
+  GLWindow::GLWindow() = default;
 
-int GLWindow::init(const char *title, int width, int height)
-{
-  glfwInit();
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  GLWindow::~GLWindow() { cleanup(); };
 
-  window = glfwCreateWindow(width, height, title, nullptr, nullptr);
-  if (window == nullptr) {
-    std::cout << "Failed to create GLFW window" << '\n';
+  int GLWindow::init(const char *title, int width, int height)
+  {
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    window = glfwCreateWindow(width, height, title, nullptr, nullptr);
+    if (window == nullptr) {
+      std::cout << "Failed to create GLFW window" << '\n';
+      glfwTerminate();
+      return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+      std::cout << "Failed to initialize GLAD" << '\n';
+      return -1;
+    }
+
+    glViewport(0, 0, width, height);
+
+
+    // Window resize Callback
+    glfwSetFramebufferSizeCallback(window, resizeWindowCallback);
+
+    return 0;
+  }
+
+  void GLWindow::update(const function<int()> &updateCallback)
+  {
+    while (!glfwWindowShouldClose(window)) {
+      processInput();
+
+      // Render something here
+      glClearColor(0.7f, 0.2f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
+      updateCallback();
+
+      glfwSwapBuffers(window);
+      glfwPollEvents();
+    }
+  }
+
+  void GLWindow::resizeWindowCallback(GLFWwindow * /*window*/, int width, int height) {
+    glViewport(0, 0, width, height);
+  }
+
+  void GLWindow::processInput()
+  {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, true);
+    }
+  }
+
+  void GLWindow::cleanup() {
     glfwTerminate();
-    return -1;
   }
-  glfwMakeContextCurrent(window);
-
-
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-    std::cout << "Failed to initialize GLAD" << '\n';
-    return -1;
-  }
-
-  glViewport(0, 0, width, height);
-
-
-  // Window resize Callback
-  glfwSetFramebufferSizeCallback(window, resizeWindowCallback);
-
-  return 0;
-}
-
-void GLWindow::update(const function<int()> &updateCallback)
-{
-  while (!glfwWindowShouldClose(window)) {
-    processInput();
-
-    // Render something here
-    updateCallback();
-
-    glfwSwapBuffers(window);
-    glfwPollEvents();
-  }
-}
-
-void GLWindow::resizeWindowCallback(GLFWwindow * /*window*/, int width, int height) {
-  glViewport(0, 0, width, height);
-}
-
-void GLWindow::processInput()
-{
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
-    glfwSetWindowShouldClose(window, true);
-  }
-}
-
-void GLWindow::cleanup() { glfwTerminate(); }
 }// namespace Argo::System
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,6 @@
 // This works but needs a delta value param
 int updateCallback()
 {
-  glClearColor(0.7f, 0.2f, 0.3f, 1.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
-
   return 0;
 }
 
