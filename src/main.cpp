698022e37d0e88b9bc23691f9f2a72ef08686f61
Kylie Benfield <kylie@revelryplay.com>
2023-10-16 18:02:26 -0600

    Simplifying GLGame call in main

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 
 #include <iostream>
 
+using namespace Argo::System;
+
 int main(int /*unused*/, char ** /*unused*/)
 {
-    Argo::System::GLGame game = *new Argo::System::GLGame();
+    auto game = *new GLGame();
 
     if (!game.init()) {
       fprintf(stderr, "Failed to initialize the game. See the logs about for more details.");
