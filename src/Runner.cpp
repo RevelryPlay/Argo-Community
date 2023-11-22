c569cb81a2d6b7d5f12f63fc0ad1aa561ed50204
Kylie Benfield <kylie@revelryplay.com>
2023-11-21 19:09:42 -0700

    Refactor Runner class implementation and header
    The implementation of the Runner class methods has been moved from Runner.hpp to Runner.cpp to separate declaration and definition. Also, to improve code readability and maintenance, the function bodies are now separate from the method declaration.

--- src/Runner.cpp
+++ src/Runner.cpp
@@ -1,4 +1,63 @@
-
 #include "Runner.hpp"
 
-namespace Argo {}  // namespace Argo
+namespace Argo {
+int Runner::run() {
+    camera.width = 300;
+    camera.height = 200;
+    camera.xPos = 10;
+    camera.yPos = 20;
+
+    scene.width = 12000;
+    scene.height = 500;
+
+    entity.height = 100;
+    entity.width = 50;
+    entity.xPos = 100;
+    entity.yPos = 75;
+
+    sprite.height = 100;
+    sprite.width = 50;
+
+    game.activeScene = &scene;
+    scene.camera = &camera;
+    entity.sprite = &sprite;
+    scene.entities.push_back( &entity );
+
+    if ( !game.Setup( Common::DEFAULT_WINDOW_TITLE,
+             Common::DEFAULT_WINDOW_WIDTH,
+             Common::DEFAULT_WINDOW_HEIGHT,
+             setup_callback ) ) {
+        fprintf( stderr, "Failed to initialize the game. See the logs about for more details." );
+        return -1;
+    }
+
+    game.Run( run_callback, update_callback, delta_callback );
+
+    return 0;
+};
+
+int Runner::setup_callback() {
+    // fprintf( stdout, "initCallback\n" );
+    return 0;
+};
+
+int Runner::run_callback() {
+    // fprintf( stdout, "runCallback\n" );
+    return 0;
+};
+
+int Runner::update_callback( float deltaTime ) {
+    // cout << "delta:" << deltaTime << '\n';
+    // fprintf( stdout, "updateCallback\n" );
+    return 0;
+};
+
+int Runner::delta_callback( float deltaTime ) {
+    // cout << "delta:" << deltaTime << '\n';
+    // fprintf( stdout, "deltaCallback\n" );
+    glClearColor( 0.3f, 0.3f, 0.5f, 1.0f );
+    glClear( GL_COLOR_BUFFER_BIT );
+    return 0;
+};
+
+}  // namespace Argo
--- src/Runner.hpp
+++ src/Runner.hpp
@@ -3,43 +3,9 @@
 #include "System/Game.hpp"
 
 namespace Argo {
-
 class Runner {
   public:
-    int run() {
-        camera.width = 300;
-        camera.height = 200;
-        camera.xPos = 10;
-        camera.yPos = 20;
-
-        scene.width = 12000;
-        scene.height = 500;
-
-        entity.height = 100;
-        entity.width = 50;
-        entity.xPos = 100;
-        entity.yPos = 75;
-
-        sprite.height = 100;
-        sprite.width = 50;
-
-        game.activeScene = &scene;
-        scene.camera = &camera;
-        entity.sprite = &sprite;
-        scene.entities.push_back( &entity );
-
-        if ( !game.Setup( Common::DEFAULT_WINDOW_TITLE,
-                 Common::DEFAULT_WINDOW_WIDTH,
-                 Common::DEFAULT_WINDOW_HEIGHT,
-                 setup_callback ) ) {
-            fprintf( stderr, "Failed to initialize the game. See the logs about for more details." );
-            return -1;
-        }
-
-        game.Run( run_callback, update_callback, delta_callback );
-
-        return 0;
-    }
+    int run();
 
   private:
     System::Game< System::GLGame > game = *new System::Game< System::GLGame >();
@@ -49,31 +15,9 @@ class Runner {
     Graphics::GLEntity entity = *new Graphics::GLEntity();
     Graphics::GLSprite sprite = *new Graphics::GLSprite();
 
-    static int setup_callback() {
-        // fprintf( stdout, "initCallback\n" );
-
-        return 0;
-    }
-
-    static int run_callback() {
-        // fprintf( stdout, "runCallback\n" );
-        return 0;
-    };
-
-    static int update_callback( float deltaTime ) {
-        // cout << "delta:" << deltaTime << '\n';
-        // fprintf( stdout, "updateCallback\n" );
-        return 0;
-    };
-
-    static int delta_callback( float deltaTime ) {
-        // cout << "delta:" << deltaTime << '\n';
-        // fprintf( stdout, "deltaCallback\n" );
-
-        glClearColor( 0.3f, 0.3f, 0.5f, 1.0f );
-        glClear( GL_COLOR_BUFFER_BIT );
-        return 0;
-    };
+    static int setup_callback();
+    static int run_callback();
+    static int update_callback( float deltaTime );
+    static int delta_callback( float deltaTime );
 };
-
 }  // namespace Argo
