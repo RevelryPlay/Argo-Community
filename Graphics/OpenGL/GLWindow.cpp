03caba11d2b4c5d6801efc791c60ed4878e649c9
Kylie Benfield <kylie@revelryplay.com>
2023-12-23 12:26:49 -0700

    Bumping OpenGL version. MacOS only supports up to 4.1 which was released in 2011.

--- Graphics/OpenGL/GLWindow.cpp
+++ Graphics/OpenGL/GLWindow.cpp
@@ -6,8 +6,8 @@ GLWindow::~GLWindow() { cleanup(); };
 
 int GLWindow::init( const char *title, const int width, const int height ) {
     glfwInit();
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 3 );
-    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 3 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MAJOR, 4 );
+    glfwWindowHint( GLFW_CONTEXT_VERSION_MINOR, 1 );
     glfwWindowHint( GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE );
     glfwWindowHint( GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE );
 
